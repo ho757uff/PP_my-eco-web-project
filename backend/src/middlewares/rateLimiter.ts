b0ddcbd15a37b85f022f1e6f81709db952bf9bbb
ho757uff <robindebessel.pro@gmail.com>
2024-06-06 18:55:35 +0200

    full setup done - plugged with PostgreSQL & Prisma

--- backend/src/middlewares/rateLimiter.ts
+++ backend/src/middlewares/rateLimiter.ts
@@ -2,20 +2,22 @@ import { Request, Response, NextFunction } from 'express';
 import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 const rateLimiter = new RateLimiterMemory({
-    points: 10,
-    duration: 1,
+  points: 10,
+  duration: 1,
 });
 
 export const rateLimiterMiddleware = (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    rateLimiter.consume(req.ip)
-        .then(() => {
-            next();
-        })
-        .catch(() => {
-            res.status(429).send('Too Many Requests');
-        });
+  const ip = req.ip || 'unknown-ip';  // Utilisation d'une valeur par défaut pour éviter l'erreur 'type undefined is not assignable to type string | number'
+  
+  rateLimiter.consume(ip)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(429).send('Too Many Requests');
+    });
 };
