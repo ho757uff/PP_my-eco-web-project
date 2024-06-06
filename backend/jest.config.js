f8343b9b4fce6131c8ce122ad178a3f301e6fb14
ho757uff <robindebessel.pro@gmail.com>
2024-06-06 20:55:19 +0200

    UPDATED: jest.config.js, tsconfig.json for workflow job run not to fail on GitHub - 4

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 };
--- backend/tests/sample.test.ts
+++ backend/tests/sample.test.ts
@@ -1,3 +1,5 @@
+/// <reference types="jest" />
+
 describe('Sample Test', () => {
   it('should pass', () => {
     expect(true).toBe(true);
